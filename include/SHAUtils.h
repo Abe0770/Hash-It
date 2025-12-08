ecde74ba85836c6a34f3d98c59f51c437852d01c
Abel Johnson <abeljohnson207@gmail.com>
2025-12-08 20:29:41 +0530

    Added SHA-512

--- include/SHAUtils.h
+++ include/SHAUtils.h
@@ -15,6 +15,9 @@ namespace sha_utils {
     unsigned long toULong(const string &str);
     unsigned long rotateLeft(unsigned long value, int bits);
     string padMessage(const std::string& input, size_t blockSize, size_t lengthSize);
+    
+    unsigned long long toULL(const string &str);
+    unsigned long long rotateLeft(unsigned long long value, int bits);
 }
 
 #endif
--- src/TerminalUtils.cpp
+++ src/TerminalUtils.cpp
@@ -1,12 +1,17 @@
 // TerminalUtils.cpp
 
 #include "TerminalUtils.h"
+#ifndef _WIN32
 #include <termios.h>
 #include <unistd.h>
+#endif
 #include <cstdio>
 
 namespace terminal {
     bool clearICanon() {
+#ifdef _WIN32
+        return true; // No-op for Windows for now
+#else
         struct termios settings;
         if (tcgetattr(STDIN_FILENO, &settings) < 0) {
             std::perror("tcgetattr failed");
@@ -18,5 +23,6 @@ namespace terminal {
             return false;
         }
         return true;
+#endif
     }
 }
